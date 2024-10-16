423566529a342510a40d85144d7f6156691ec5b8
DavidPang3 <dpang3@buffalo.edu>
2024-10-16 10:08:32 -0400

    updated shark.py

--- .venv/shark.py
+++ .venv/shark.py
@@ -3,7 +3,8 @@ import time
 import tkinter as tk
 import threading
 import keyboard
-import pyscreeze
+from numpy import random
+import easyocr
 
 
 
@@ -12,60 +13,81 @@ pyautogui.FAILSAFE=True
 screenWidth, screenHeight = pyautogui.size() 
 currentMouseX, currentMouseY = pyautogui.position() 
 
+print(f"Current screen width: {screenWidth}\n Current screen height: {screenHeight}")
+print(f"mouse x position: {currentMouseX}\n mouse y position: {currentMouseY}")
+
+#autoclicker
 running = False
 timebool = False
-screenshot = pyautogui.screenshot('disconnect.png')
+screenshot = False
 timecounter = 1
 holder = False
 threadfarm = None
 threadcounter = None
 
-def checkdisconnect():
-
+def beginfarm():
+    global timecounter
     global running
+    global timebool
     running = True
     while(running):
+        time.sleep(1)
+        print("code not implemented yet\n")
 
-        try:
-            location = pyautogui.locateOnScreen('disconnect.png')
-            print(f"location={location}")
-        except pyautogui.ImageNotFoundException:
-            print("Image not found, retrying...")
-            time.sleep(3)
-      
-        
-def endcheck():
+def endfarm():
     global running
     running = False
 
-def start_check_thread():
+def start_farm_thread():
     global threadfarm 
+    global timecounter
     if(threadfarm == None):
-        threadfarm = threading.Thread(target=checkdisconnect)
+        threadfarm = threading.Thread(target=beginfarm)
         threadfarm.daemon = True
         threadfarm.start()
     elif(threadfarm.is_alive()==False):
-        threadfarm = threading.Thread(target=checkdisconnect)
+        threadfarm = threading.Thread(target=beginfarm)
         threadfarm.daemon = True
         threadfarm.start()
 
 def count_time():
     global timebool
     timebool = True
+    global timecounter
     timecounter = 1
     while(timebool):
         timecounter+=1
         print(f"timecounter = {timecounter}")
         time.sleep(1)
-        if(timecounter%20==0):
-            keyboard.press_and_release(' ')
-        if(timecounter%9==0):
-            keyboard.press('d')
-            time.sleep(0.1)  
-            keyboard.release('d')
+        x=random.randint(6)
+        y=random.randint(6)
+        if(x==0):
+            time.sleep(3)
+        elif(x==1):
+            keyboard.press('w')
+            time.sleep(y)
+            keyboard.release('w')
+        elif(x==2):
             keyboard.press('a')
-            time.sleep(0.1)  
+            time.sleep(y)
             keyboard.release('a')
+        elif(x==3):
+            keyboard.press('s')
+            time.sleep(y)
+            keyboard.release('s')
+        elif(x==4):
+            keyboard.press('d')
+            time.sleep(y)
+            keyboard.release('d')
+        elif(x==5):
+            keyboard.press('space')
+            time.sleep(y)
+            keyboard.release('space')
+        elif(x==6):
+            for int in range(y):
+                pyautogui.click()
+                time.sleep(.1)
+        
 
 def start_time_counter_thread():
     global threadcounter
@@ -83,8 +105,8 @@ def StopAndReset_timer():
     timebool = False
 
 #you can use 'ctrl+s' for key combos
-keyboard.add_hotkey('9', start_check_thread)
-keyboard.add_hotkey('0', endcheck)  
+keyboard.add_hotkey('9', start_farm_thread)
+keyboard.add_hotkey('0', endfarm)  
 keyboard.add_hotkey('1', start_time_counter_thread)
 keyboard.add_hotkey('2', StopAndReset_timer)  
 
@@ -92,12 +114,14 @@ keyboard.add_hotkey('2', StopAndReset_timer)
 root = tk.Tk()
 root.title("AutoFarmer")
 root.geometry("400x300")
-start_button = tk.Button(root, text="Start / 9", command=start_check_thread)
+start_button = tk.Button(root, text="Auto Rejoin / 9", command=start_farm_thread)
 start_button.pack(padx=20, pady=20)
-stop_button = tk.Button(root, text="Stop / 0", command=endcheck)
+stop_button = tk.Button(root, text="Stop Rejoin / 0", command=endfarm)
 stop_button.pack(padx=20, pady=20)
-start_time = tk.Button(root, text="TimeCounter / 1", command=start_time_counter_thread)
+start_time = tk.Button(root, text="Generate Movement / 1", command=start_time_counter_thread)
 start_time.pack(padx=20, pady=20)
-stop_time = tk.Button(root, text="Stop and Reset Timer / 2", command=StopAndReset_timer)
+stop_time = tk.Button(root, text="Stop Generated Movement / 2", command=StopAndReset_timer)
 stop_time.pack(padx=20, pady=20)
 root.mainloop()
+
+
